f6d83a87a77e7720b8e81de0f05743e0045c60b4
minghao912 <minghaochen912@gmail.com>
2022-12-08 21:44:20 -0800

    feat: add TURN servers

--- pages/receive.tsx
+++ pages/receive.tsx
@@ -27,8 +27,22 @@ export default function Send() {
                 iceServers: [
                     { urls: 'stun:stun.l.google.com:19302' }, 
                     { urls: 'stun:global.stun.twilio.com:3478?transport=udp' },
-                    { urls: 'stun:stun.qq.com:3478' },
-                    { urls: 'stun:stun2.l.google.com:19302' }
+                    { urls: "stun:openrelay.metered.ca:80" },
+                    {
+                        urls: "turn:openrelay.metered.ca:80",
+                        username: "openrelayproject",
+                        credential: "openrelayproject"
+                    },
+                    {
+                        urls: "turn:openrelay.metered.ca:443",
+                        username: "openrelayproject",
+                        credential: "openrelayproject"
+                    },
+                    {
+                        urls: "turn:openrelay.metered.ca:443?transport=tcp",
+                        username: "openrelayproject",
+                        credential: "openrelayproject"
+                    }
                 ]
             }
         });
